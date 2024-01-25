06012ccc1992f806de7b92650bad7d0caa4aef71
Mehedi Ratul <mehediratul41@gmail.com>
2024-01-26 00:16:38 +0600

    bubble sort best case time complexity O(n) optimized

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -5,7 +5,7 @@ void bubble_sort(int arr[],int n)
 {
     for(int i=n-1; i>=0 ; i--)
     {
-
+        bool didAnySwap=0;
         for(int j=0; j<=i-1 ; j++)
         {
             if(arr[j+1]<arr[j])
@@ -13,9 +13,15 @@ void bubble_sort(int arr[],int n)
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                didAnySwap = 1;
             }
 
         }
+        if(!didAnySwap)
+        {
+            break;
+        }
+        cout<<"Runs " <<n-i <<" times" <<endl;
     }
     //after bubble sort
     cout<<"After bubble sort: ";
