763d322f8112a38c47b4a50c3992f31fe2b68479
Mehedi Ratul <mehediratul41@gmail.com>
2024-01-26 00:07:59 +0600

    bubble sort optimized

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 void bubble_sort(int arr[],int n)
 {
-    for(int i=0; i<n-1 ; i++)
+    for(int i=n-1; i>=0 ; i--)
     {
-        int min = i;
-        for(int j=i+1; j<n ; j++)
+
+        for(int j=0; j<=i-1 ; j++)
         {
-            if(arr[j]<arr[min])
+            if(arr[j+1]<arr[j])
             {
-                int temp = arr[min];
-                arr[min] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
 
         }
